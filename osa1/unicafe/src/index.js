67b0ccde6b8f23d7d398ba9afabf43d3057bc610
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2020-12-25 17:52:36 +0200

    Task 1.6 done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -7,9 +13,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text='good' />
+      <Button onClick={handleNeutral} text='neutral' />
+      <Button onClick={handleBad} text='bad' />
+      <h1>statistics</h1>
+      good: {good}<br/>
+      neutral: {neutral}<br/>
+      bad: {bad}<br/>
     </div>
   )
 }
