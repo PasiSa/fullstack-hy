e7ff5f08e80cfed1e5e4f5060b563521a8deb59e
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2020-12-31 11:47:41 +0200

    Updated baseUrl to reflect Heroku-deplyed server.

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+//const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://stormy-thicket-13410.herokuapp.com/api/persons'
 
 const getAll = () => {
   const req = axios.get(baseUrl)
