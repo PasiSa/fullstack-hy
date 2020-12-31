5faa21e5af4384dc732fc364f67312b86ba65371
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2020-12-31 12:11:41 +0200

    Task 3.11 done.

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 //const baseUrl = 'http://localhost:3001/api/persons'
-const baseUrl = 'https://stormy-thicket-13410.herokuapp.com/api/persons'
+//const baseUrl = 'https://stormy-thicket-13410.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const req = axios.get(baseUrl)
